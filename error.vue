645a2002a63c4eaa32384bf4863818bf7c3eefbf
isntlazy <isntlazy@gmail.com>
2022-12-16 18:06:39 +0200

    added custom 404 page

---
+++ error.vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLayout name="default">
+    <main class="px-3 md:px-6 xl:px-0">
+      <div class="flex flex-col items-center w-full justify-center">
+        <span class="bg-red-100 text-red-700 text-xl font-semibold px-4 py-1 rounded">404</span>
+
+        <h1 class="text-red-700 text-2xl uppercase mt-8">
+          Халепа! Сторінку не знайдено 😿
+        </h1>
+        <img class="w-48" src="./assets/img/page-not-found.png" alt="Іконка помилки 404">
+        <button type="button" class="flex flex-col my-12 items-center" @click="redirectToHome">
+          <span class="mb-1 text-center uppercase font-bold text-primary">Повернутись на головну</span>
+          <img class="w-16 mt-4" src="./assets/img/arrow-right.svg" alt="Стрілка перейти на головну">
+        </button>
+      </div>
+    </main>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+
+console.log(props.error)
+
+const redirectToHome = () => clearError({ redirect: '/' })
+</script>
