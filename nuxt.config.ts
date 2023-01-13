ed2dbba2edb7fb655c9a255b7c4b67705284dbaa
isntlazy <isntlazy@gmail.com>
2023-01-13 20:32:33 +0200

    changing nitro preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'netlify-edge'
+  },
   // extends: '@nuxt-themes/typography',
   // ssr: false,
   tailwindcss: {
